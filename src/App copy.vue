8aacc2678c012f2c41bae4deb623164a9fe78c3f
ZrtILee <2350775562@qq.com>
2024-05-14 18:29:48 +0800

    瀑布流初次提交

---
+++ src/App copy.vue
@@ -0,0 +1,42 @@
+<script setup>
+import 'wc-waterfall'
+
+import { ref } from 'vue'
+const  list = ref([])
+const cols = ref(Math.floor(window.innerWidth / 236))
+const colsArr = ref(new Array(cols.value).fill(0))
+
+fetch('https://mock.presstime.cn/mock/663dc355d59f9a21eb4d690b/example/photos')
+.then((res)=>{
+  res.json().then(ress=>{
+    list.value = ress.data.data
+    colsArr.value.f
+    console.log(ress)
+  })
+})
+.catch((err)=>console.log(err))
+console.log(colsArr.value)
+window.addEventListener("resize", function () {
+  console.log(window.innerWidth)
+  cols.value = Math.floor(window.innerWidth / 236)
+  colsArr.value = new Array(cols.value).fill(0)
+console.log(colsArr.value)
+
+});
+</script>
+
+<!-- App.vue -->
+<template>
+  <div>
+    <wc-waterfall gap="10" :cols="cols" v-if="list[0]" style="background-color: aqua;">
+      <img class="change" :style="{ 'padding-bottom': `${ (img.height / img.width) * 1 }`, 'background': 'pink'  }" style="" v-for="img in list" :src="img.urls.raw" />
+    </wc-waterfall>
+    <div>asd</div>
+  </div>
+</template>
+
+<style scoped>
+.change{
+  transition: all 0.3s;
+}
+</style>
---
+++ src/masonry.ts
@@ -0,0 +1,71 @@
+let grid: HTMLElement = null; // grid
+let colsCount: number = 0; // 列数
+let colsArr: number[] = []; // 列数组
+let gap: number = 0; // 间距
+let resizeListener: EventListener = null// resize监听
+
+const positionComputedX = (ref: HTMLElement) => {
+    grid = ref;// grid
+    let gridWidth = grid.clientWidth;// grid宽度
+    let marginB = 30;// 底部间距
+    let gapX: number;// 间距
+
+    for (let i of grid.children as any) {
+        let min = Math.min(...colsArr);
+        let index = colsArr.indexOf(min);
+        let clientH = i.clientHeight as number;
+        i.style.marginBottom = `${marginB}px`;
+        colsArr[index] += clientH + marginB;
+
+        switch (index) {
+            case 0:
+                gapX = gap * (3 / 8);
+                break;
+            case 1:
+                gapX = gap * (4 / 8);
+                break;
+            case 2:
+                gapX = gap * (5 / 8);
+                break;
+            default:
+                break;
+        }
+
+        if (!index) {
+            i.style.transform = `translate(${index * ((gridWidth - gap) / colsCount) + gapX}px,${min}px)`;
+        } else {
+            i.style.transform = `translate(${index * ((gridWidth - gap) / colsCount) + gapX}px,${min}px)`;
+        }
+    }
+    console.log(Math.max(...colsArr) + marginB)
+    ref.style.height = Math.max(...colsArr) + marginB + 'px'
+};
+
+const ArgComputed = () => {
+  if (window.innerWidth < 731) {
+    gap = 0;
+    colsArr = new Array(1).fill(0);
+    colsCount = 1;
+  } else if (window.innerWidth < 1280) {
+    window.innerWidth < 961 ? (gap = 64) : (gap = 112);
+    colsArr = new Array(2).fill(0);
+    colsCount = 2;
+  } else {
+    window.innerWidth < 1460 ? (gap = 128) : (gap = 192);
+    colsArr = new Array(3).fill(0);
+    colsCount = 3;
+  }
+};
+
+ArgComputed();
+
+if(!resizeListener){
+    window.addEventListener("resize", () => {
+        ArgComputed();
+        positionComputedX(grid);
+    });
+}
+
+
+
+export default positionComputedX
